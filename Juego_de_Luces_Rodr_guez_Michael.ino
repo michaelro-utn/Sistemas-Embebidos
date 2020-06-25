a9e72d18b65f4d34a36a64a61cd65d42580ed0ff
Michael Rodríguez <57365544+michaelro-utn@users.noreply.github.com>
2020-06-25 12:35:37 -0500

    Juego_Luces_RodríguezMichael

--- Juego_de_Luces_Rodr_guez_Michael.ino
+++ Juego_de_Luces_Rodr_guez_Michael.ino
@@ -9,7 +9,7 @@ const int vector[6]={8,9,10,11,12,13};
 const int btn=7;
 int i=0;
 int j;
-int p;
+int n;
 const int led1=8;
 const int led2=9;
 const int led3=10;
@@ -17,8 +17,7 @@ const int led4=11;
 const int led5=12;
 const int led6=13;
 const int btn1=6; 
-int k; 
-int l; 
+int k;  
 int state=0; 
 void setup() {
   // put your setup code here, to run once:
@@ -38,11 +37,7 @@ void setup() {
 
 void loop() {
   if (digitalRead (btn)== HIGH){
-    delay (300);
-    state=1-state;
-    if(state==1){
-    while (j<=7){
-      j++;
+    for (n=0;n<=1;n++){
         for(i=0;i<6;i++){
           digitalWrite(vector[i], HIGH);
           delay(150);
@@ -50,24 +45,11 @@ void loop() {
           delay(150);
       }
     }
-    }else{
-    while (p<=7){
-      p++;
-        for(i=0;i<6;i++){
-          digitalWrite(vector[i], HIGH);
-          delay(200);
-          digitalWrite(vector[i],LOW);
-          delay(200);
-        }      
-      }
     }
-  }
+ //Boton2 
+ 
   if (digitalRead (btn1)==HIGH){
-    delay(300);
-    state=1-state;
-    if (state==1){
-      while (k<=9){
-        k++;
+      for (k=0;k<=1;k++){
         digitalWrite (led1, HIGH);
         digitalWrite (led6, HIGH);
         delay(200); 
@@ -86,29 +68,6 @@ void loop() {
         digitalWrite (led3,LOW);
         digitalWrite (led4,LOW);
         delay (200);
-      }
-      }else{ 
-        while (l<=9){
-          l++;
-          digitalWrite (led1, HIGH);
-          digitalWrite (led6, HIGH);
-          delay(200); 
-          digitalWrite (led1,LOW);
-          digitalWrite (led6,LOW);
-          delay (200);
-          digitalWrite (led2, HIGH);
-          digitalWrite (led5, HIGH);
-          delay(200); 
-          digitalWrite (led2,LOW);
-          digitalWrite (led5,LOW);
-          delay (200);
-          digitalWrite (led3, HIGH);
-          digitalWrite (led4, HIGH);
-          delay(200); 
-          digitalWrite (led3,LOW);
-          digitalWrite (led4,LOW);
-          delay (200);
-      }
     }
   }
 }
