86252eb7fa020935af6786c15eefe9e56cd94233
Michael Rodríguez <57365544+michaelro-utn@users.noreply.github.com>
2020-07-02 22:00:26 -0500

    CódigoMaster_ValidacionClave

---
+++ Validacion_clave.ino
@@ -0,0 +1,32 @@
+#include <Wire.h>
+String contra;
+int tama;
+int i=0;
+String sal;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available()>0){
+    contra=Serial.readString();
+    Serial.println("");
+    contra.toLowerCase();
+//    tama=contra.length();
+//    char vector[tama];
+//    contra.toCharArray(vector,tama);
+//    Serial.println("Contraseña Ingresada!")
+    if (contra=="avion"){
+      sal="1111";
+    }else{
+      sal="0000";
+    }
+    Serial.println("Clave codificada");
+    Serial.println(sal);
+    Wire.beginTransmission(4);
+    Wire.write(sal.c_str());
+    Wire.endTransmission();
+  }
+}
